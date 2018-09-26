5490e3aeecafb7a2bdc2d80a384b11e29eb02ddd
unknown <viclec@yahoo.gr>
2018-09-27 00:43:57 +0300

    Added getter

--- src/store.js
+++ src/store.js
@@ -5,18 +5,15 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    color: '',
-    repo: ''
+    
   },
   mutations: {
-    mutateRepo: function (state, payload) {
-      state.repo = payload
-   }
+    
   },
   actions: {
-    updateRepo: function ({commit}, payload) {
-      console.log('feafae')
-      commit('mutateRepo', payload)
-    }
+    
+  },
+  getters: {
+    
   }
 })
