eacf50e3e4a49b18b22245c68fc8af6915fa8201
unknown <viclec@yahoo.gr>
2018-09-27 00:42:33 +0300

    Added font awesome

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
 import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
 
@@ -14,6 +19,10 @@ Vue.use(Vuetify)
 Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
+
+library.add(fas, fab)
+
+Vue.component('fa', FontAwesomeIcon)
 
 new Vue({
   router,
